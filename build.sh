ee0e0e2ce66b3063cc52339ab40f4bad80c28ae6
zanpocc <980925441@qq.com>
2022-01-04 15:14:55 +0800

    add comment

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 source ./env.bash
 ./configure clean
 #./configure OBJCOPY=gobjcopy --with-debug-level=slowdebug --enable-debug-symbols ZIP_DEBUGINFO_FILES=0
-./configure --with-debug-level=slowdebug --enable-debug-symbols --with-jvm-variants=server --with-target-bits=64 --with-freetype-include=/usr/include/freetype2 --with-freetype-lib=/usr/lib/x86_64-linux-gnu
+./configure --with-debug-level=slowdebug --with-native-debug-symbols=internal --disable-zip-debug-info --enable-debug-symbols --with-jvm-variants=server --with-target-bits=64 --with-freetype-include=/usr/include/freetype2 --with-freetype-lib=/lib64
+make
+
+# 搜索库文件命令
+#ldconfig -p | grep libfreetype
+
 # 使用与平台无关的解释器
 # ./configure -zero --with-debug-level=slowdebug --enable-debug-symbols --with-jvm-variants=server --with-target-bits=64 --with-freetype-include=/usr/include/freetype2 --with-freetype-lib=/usr/lib/x86_64-linux-gnu
-make
-rm -rf ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server/libjvm.debuginfo
-unzip ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server/libjvm.diz -d ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server
+
+# 解压符号文件
+#rm -rf ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server/libjvm.debuginfo
+#unzip ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server/libjvm.diz -d ./build/linux-x86_64-normal-server-slowdebug/jdk/lib/amd64/server
